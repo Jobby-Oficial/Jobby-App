4b631bff92593a0dff778503d07ee91a460e7d3f
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-10-25 15:57:54 +0100

    Recyclerview no CategoryFragment e o CategoryAdapter

--- jobby-app/app/src/main/java/com/example/jobby_oficial/CategoryFragment.java
+++ jobby-app/app/src/main/java/com/example/jobby_oficial/CategoryFragment.java
@@ -3,10 +3,14 @@ package com.example.jobby_oficial;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class CategoryFragment extends Fragment {
 
@@ -15,6 +19,8 @@ public class CategoryFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+    RecyclerView rvCategory;
+    ArrayList<CategoryClass> arrayList_category;
 
     public CategoryFragment() {
         // Required empty public constructor
@@ -41,7 +47,26 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        rvCategory = view.findViewById(R.id.recyclerView_category);
+        rvCategory.setLayoutManager(new LinearLayoutManager(getContext()));
+        arrayList_category = new ArrayList<>();
+        CategoryClass category1 = new CategoryClass(R.drawable.ic_topic,"Categoria 1"," Descrisão 1");
+        CategoryClass category2 = new CategoryClass(R.drawable.ic_topic,"Categoria 2"," Descrisão 2");
+        CategoryClass category3 = new CategoryClass(R.drawable.ic_topic,"Categoria 3"," Descrisão 3");
+        CategoryClass category4 = new CategoryClass(R.drawable.ic_topic,"Categoria 4"," Descrisão 4");
+        CategoryClass category5 = new CategoryClass(R.drawable.ic_topic,"Categoria 5"," Descrisão 5");
+        CategoryClass category6 = new CategoryClass(R.drawable.ic_topic,"Categoria 6"," Descrisão 6");
+        CategoryClass category7 = new CategoryClass(R.drawable.ic_topic,"Categoria 7"," Descrisão 7");
+        arrayList_category.add(category1);
+        arrayList_category.add(category2);
+        arrayList_category.add(category3);
+        arrayList_category.add(category4);
+        arrayList_category.add(category5);
+        arrayList_category.add(category6);
+        arrayList_category.add(category7);
+
+        rvCategory.setAdapter(new CategoryAdapter(arrayList_category));
+        return view;
     }
 }
