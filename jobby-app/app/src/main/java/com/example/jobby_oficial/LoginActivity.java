c119965faf9f5e2f6775f3be57ca0f93c68de38f
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-29 18:00:52 +0000

    Login design update (button go to register)

--- jobby-app/app/src/main/java/com/example/jobby_oficial/LoginActivity.java
+++ jobby-app/app/src/main/java/com/example/jobby_oficial/LoginActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class LoginActivity extends AppCompatActivity {
 
     LottieAnimationView lavBack;
+    Button btnGoToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +29,18 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(LoginActivity.this,RegisterStepOneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void InitControls() {
         lavBack = findViewById(R.id.lav_back_login);
+        btnGoToRegister = findViewById(R.id.btn_go_to_register);
     }
 }
