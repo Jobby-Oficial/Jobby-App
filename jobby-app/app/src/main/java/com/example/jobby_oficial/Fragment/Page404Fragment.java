6cc312555627c58830bfd71d1bd00dee15f9f69e
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2023-03-08 22:11:05 +0000

    Custom 404 Page Not Found

---
+++ jobby-app/app/src/main/java/com/example/jobby_oficial/Fragment/Page404Fragment.java
@@ -0,0 +1,56 @@
+/*
+ * Created by Guilherme Cruz
+ * Last modified: 02/01/22, 01:05
+ * Copyright (c) 2022.
+ * All rights reserved.
+ */
+
+package com.example.jobby_oficial.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jobby_oficial.R;
+
+public class Page404Fragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public Page404Fragment() {
+        // Required empty public constructor
+    }
+
+    public static Page404Fragment newInstance(String param1, String param2) {
+        Page404Fragment fragment = new Page404Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page404, container, false);
+    }
+}
