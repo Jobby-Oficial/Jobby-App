bdaeb50dbd8838ab1e8adfac8790ab33ec81a38c
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2023-05-03 20:00:03 +0100

    Page 404 Not Found Animations

--- jobby-app/app/src/main/java/com/example/jobby_oficial/Fragment/Page404Fragment.java
+++ jobby-app/app/src/main/java/com/example/jobby_oficial/Fragment/Page404Fragment.java
@@ -14,8 +14,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.jobby_oficial.R;
 
 public class Page404Fragment extends Fragment {
@@ -26,7 +29,8 @@ public class Page404Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    LottieAnimationView imgSparklesCategory;
+    LottieAnimationView imgPage404NotFound;
+    TextView tvNoResultFound404, tvContentPage404Found;
 
     public Page404Fragment() {
         // Required empty public constructor
@@ -54,6 +58,18 @@ public class Page404Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page404, container, false);
+        View view = inflater.inflate(R.layout.fragment_page404, container, false);
+        InitControls(view);
+        YoYo.with(Techniques.FadeInUp).duration(3000).repeat(0).playOn(imgPage404NotFound);
+        YoYo.with(Techniques.FadeInUp).duration(3000).repeat(0).playOn(tvNoResultFound404);
+        YoYo.with(Techniques.FadeInUp).duration(3000).repeat(0).playOn(tvContentPage404Found);
+
+        return view;
+    }
+
+    private void InitControls(View view) {
+        imgPage404NotFound = view.findViewById(R.id.lav_page_404_not_found);
+        tvNoResultFound404 = view.findViewById(R.id.tv_page_404_not_found);
+        tvContentPage404Found = view.findViewById(R.id.tv_content_page_404);
     }
 }
